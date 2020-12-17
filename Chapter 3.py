6ba6574e08ef3db3587635a26c54bfa547931b49
NikolayTheBoss <tnn80@yandex.ru>
2020-12-17 17:53:43 +1000

    Добавил задание 4

--- Chapter 3.py
+++ Chapter 3.py
@@ -8,12 +8,25 @@ Created on Wed Dec 16 13:41:44 2020
 
 
 def task_4():
-    pass
+    L = []
+    for i in range(6):
+        L.append(2**i)
+    X = 5
+    found = False
+    i = 0
+    while not found and i < len(L):
+        if 2 ** X == L[i]:
+            found = True
+        else:
+            i = i + 1
+    if found:
+        print('at index', i)
+    else:
+        print(X, 'not found')
 
 
 def main():
     task_4()
-    pass
 
 
 if __name__ == '__main__':

6bb695f50d7177f28fba066be10c739a47ab987f
NikolayTheBoss <tnn80@yandex.ru>
2020-12-25 09:12:35 +1000

    Готовые модули TIMER и TIMESEQES

--- timer.py
+++ timer.py
@@ -7,27 +7,29 @@
 
 import time
 import sys
-from permute import permute2
+# from permute import permute2
 
-timer = time.perf_counter if sys.platform[:3] == 'win' else time.time
+timer = time.perf_counter
 
 
-def total(reps, func, * pargs, ** kargs):
+def total(reps, func, *pargs, **kargs):
     """
     Суммарное время выполнения функции func() reps раз
+
     Возвращает (суммарное время, последний результат)
     """
-    repslist = list (range(reps))     # Уравнять Python 2.x, 3.x
+    repslist = list(range(reps))     # Уравнять Python 2.x, 3.x
     start = timer()                   # Или perf—Counter/другая в Python 3.3+
     for i in repslist:
-        ret = func(* pargs, ** kargs)
+        ret = func(*pargs, **kargs)
         elapsed = timer() - start
     return (elapsed, ret)
 
 
-def bestof(reps, func, * pargs, ** kargs):
+def bestof(reps, func, *pargs, **kargs):
     """
     Самая быстрая функция func() среди reps запусков.
+
     Возвращает (лучшее время, последний результат)
     """
     best = 2**32          # 136 лет представляется достаточным
@@ -40,13 +42,9 @@ def bestof(reps, func, * pargs, ** kargs):
     return (best, ret)
 
 
-def bestoftotal(reps1, reps2, func, * pargs, ** kargs):
+def bestoftotal(reps1, reps2, func, *pargs, **kargs):
     """
     Лучшее суммарное время:
     (лучшее время из reps1 запусков (суммарное время reps2 запусков func))
     """
-    return bestof(reps1, total, reps2, func, * pargs, ** kargs)
-
-
-if __name__ == '__main__':
-    print(total(1, permute2('123')))
+    return bestof(reps1, total, reps2, func, *pargs, **kargs)
---
+++ timeseqs.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Проверка относительной скорости итерационных альтернатив.
+Файл timeseqs.py
+"""
+
+
+import sys
+import timer
+
+reps = 10000
+repslist = list(range(reps))
+
+
+def forLoop() -> list:
+    """Генерирует список циклом и добавлением append."""
+    res = []
+    for i in repslist:
+        res.append(abs(i))
+    return res
+
+
+def listComp() -> list:
+    """Генерирует список включением списка."""
+    return [abs(i) for i in repslist]
+
+
+def mapList() -> list:
+    """Генерирует список отображением map"""
+    return list(map(abs, repslist))
+
+
+def genExp() -> list:
+    """Генерирует список проходом цикла"""
+    return list(abs(i) for i in repslist)
+
+
+def genFunc() -> list:
+    """Генерирует список преобразованием геренатора в list"""
+    def gen():
+        for i in repslist:
+            yield abs(i)
+    return list(gen())
+
+
+if __name__ == '__main__':
+    for testFunc in (forLoop, listComp, mapList, genExp, genFunc):
+        (bestof, (total, result)) = timer.bestoftotal (5, 1000, testFunc)
+        print ('%-9s: %.5f => [%s...%s]' % 
+           (testFunc.__name__ , bestof, result[0], result[-1]))
